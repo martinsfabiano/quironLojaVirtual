41df45b82c300550b91c3008f216e8fdf632233b
martinsfabiano <martins.fabiano@gmail.com>
2016-09-03 23:03:52 -0300

    Teste paginação

--- Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
+++ Quiron.LojaVirtual.UnitTest/UnitTestQuiron.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiron.LojaVirtual.Web.HtmlHelpers;
+using Quiron.LojaVirtual.Web.Models;
 
 namespace Quiron.LojaVirtual.UnitTest
 {
@@ -7,8 +10,25 @@ namespace Quiron.LojaVirtual.UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestePaginacao()
         {
+            HtmlHelper html = null;
+            Paginacao paginacao = new Paginacao
+            {
+                PaginaAtual = 2,
+                ItensTotal = 28,
+                ProdutosPorPagina = 10
+            };
+
+            Func<int, string> paginaUrl = i => "Pagina" + i;
+
+            MvcHtmlString resultado = html.PageLinks(paginacao,paginaUrl);
+            
+            Assert.AreEqual(
+                 @"<a class=""btn btn-default"" href=""Pagina1"">1</a>"
+                 + @"<a class=""btn btn-default btn-primary selected"" href=""Pagina2"">2</a>"
+                 + @"<a class=""btn btn-default"" href=""Pagina3"">3</a>", resultado.ToString()
+                 );
         }
     }
 }
---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        public int ItensTotal { get; set; }
+        public int ProdutosPorPagina { get; set; }
+        public int PaginaAtual { get; set; }
+        public int TotalPaginas
+        {
+            get { return (int)Math.Ceiling((decimal)ItensTotal / ProdutosPorPagina); }
+        }
+    }
+}
